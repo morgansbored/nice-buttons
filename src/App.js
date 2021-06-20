be8bab3427206c48216226e0d87ab438dc0412ab
“morganpearse” <morgan@morganpearse.com>
2021-06-20 19:19:09 +0100

    added more buttons to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './Button';
-import { ButtonSizes, ButtonTypes } from './buttonTypes';
+import { ButtonSizes, ButtonTypes, ButtonThemes } from './buttonTypes';
 import { IconTypes } from "./iconTypes";
 import "./styles.css";
 export const App = () => {
@@ -72,6 +72,83 @@ export const App = () => {
                 type={ButtonTypes.SECONDARY}
                 />
             </div>
+
+            <div className="App dark">
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.PRIMARY}
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.PRIMARY}
+                icon={IconTypes.SAVE}
+                theme={ButtonThemes.DARK}
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.PRIMARY}
+                disabled
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.SECONDARY}
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.SECONDARY}
+                disabled
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.TERTIARY}
+                />
+                <Button
+                size={ButtonSizes.MEDIUM}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.TERTIARY}
+                disabled
+                />
+                <Button
+                size={ButtonSizes.SMALL}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.PRIMARY}
+                />
+                <Button
+                size={ButtonSizes.LARGE}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.PRIMARY}
+                />
+                <Button
+                size={ButtonSizes.LARGE}
+                label="Button"
+                theme={ButtonThemes.DARK}
+                onClickHandler={() => alert("you clicked!")}
+                type={ButtonTypes.SECONDARY}
+                />
+          </div>
         </React.Fragment>
     );
 };
